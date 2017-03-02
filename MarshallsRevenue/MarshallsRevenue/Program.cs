cc03d6bc6b6aa866ed8ac4ece03654023b3d14e3
David Brunow <dbrunow@nctc.edu>
2017-03-02 17:08:35 -0600

    Write program

--- MarshallsRevenue/MarshallsRevenue/Program.cs
+++ MarshallsRevenue/MarshallsRevenue/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Console;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,30 @@ namespace MarshallsRevenue
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            const double INTERIOR_MURAL_COST = 500;
+            const double EXTERIOR_MURAL_COST = 750;
+
+            int numberOfInteriorMurals = 0;
+            int numberOfExteriorMurals = 0;
+
+            Write("Enter the number of interior murals estimated for next month: ");
+            numberOfInteriorMurals = int.Parse(ReadLine());
+
+            Write("Enter the number of exterior murals estimated for next month: ");
+            numberOfExteriorMurals = int.Parse(ReadLine());
+
+            WriteLine("\nThanks!\n\nHere are your results: ");
+
+            double interiorMuralRevenue = numberOfInteriorMurals * INTERIOR_MURAL_COST;
+            double exteriorMuralRevenue = numberOfExteriorMurals * EXTERIOR_MURAL_COST;
+
+            WriteLine("Revenue for {0} interior murals: {1}", numberOfInteriorMurals, interiorMuralRevenue);
+            WriteLine("Revenue for {0} exterior murals: {1}", numberOfExteriorMurals, exteriorMuralRevenue);
+            WriteLine("Total Estimated Revenue: {0}", interiorMuralRevenue + exteriorMuralRevenue);
+
+            WriteLine("More exterior murals are scheduled than interior: {0}", numberOfExteriorMurals > numberOfInteriorMurals);
         }
     }
 }
